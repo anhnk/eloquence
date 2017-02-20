3179acf062b76f201da071a3b920be986259139c
Anh Do <dotuanhoanganh@gmail.com>
2017-02-20 21:15:51 +1100

    Turn NoteForm into react component

--- web/static/js/container/note/form.js
+++ web/static/js/container/note/form.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 
-export default React.createClass({
-  render: function() {
-      return (
-        <div className="ui grid container">
-          <div className="sixteen wide column">
-            <h2>New entry</h2>
-            <form className='ui form'>
-              <div className='field'>
-                <label>Body</label>
-                <textarea name='body' placeholder='Body'></textarea>
-              </div>
-              <div className='field'>
-                <label>Source</label>
-                <input type='text' name='source' placeholder='Source'/>
-              </div>
-              <button className='ui button' type='submit'>Submit</button>
-            </form>
-          </div>
+export default class NoteForm extends Component {
+  render() {
+    return (
+      <div className="ui grid container">
+        <div className="sixteen wide column">
+          <h2>New entry</h2>
+          <form className='ui form' onSubmit={::this._handleSubmit}>
+            <div className='field'>
+              <label>Body</label>
+              <textarea name='body' placeholder='Body'></textarea>
+            </div>
+            <div className='field'>
+              <label>Source</label>
+              <input type='text' name='source' placeholder='Source'/>
+            </div>
+            <button className='ui button' type='submit'>Submit</button>
+          </form>
         </div>
-      );
-    }
-});
+      </div>
+    );
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    browserHistory.push('/');
+  }
+}
