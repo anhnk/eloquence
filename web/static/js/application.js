b99a3d9deb2cc5c750bac3cd90f4fa704b1bcd0a
Anh Do <dotuanhoanganh@gmail.com>
2017-02-20 20:12:36 +1100

    Move new entry container to a separate file

--- web/static/js/application.js
+++ web/static/js/application.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, IndexLink, Link, browserHistory } from 'react-router';
+import NoteForm from './container/note/form';
 
 let App = React.createClass({
   render: function() {
@@ -22,39 +23,21 @@ var Home = React.createClass({
           <div className="sixteen wide column"><button className="ui fluid button">Add a word</button></div>
           <div className="sixteen wide column"><button className="ui fluid button">Review vocabulary</button></div>
 
-          <div className="sixteen wide column"><Link to="/stuff"><button className="ui fluid button">Add a note</button></Link></div>
+          <div className="sixteen wide column"><Link to="/notes/new"><button className="ui fluid button">Add a note</button></Link></div>
           <div className="sixteen wide column"><a href="/entries"><button className="ui fluid button">Review notes</button></a></div>
         </div>
       );
     }
 });
 
-var Stuff = React.createClass({
-  render: function() {
-      return (
-        <div>
-          <h2>STUFF</h2>
-          <p>Mauris sem velit, vehicula eget sodales vitae,
-          rhoncus eget sapien:</p>
-          <ol>
-            <li>Nulla pulvinar diam</li>
-            <li>Facilisis bibendum</li>
-            <li>Vestibulum vulputate</li>
-            <li>Eget erat</li>
-            <li>Id porttitor</li>
-          </ol>
-        </div>
-      );
-    }
-});
-
-
 const target = document.getElementById('main-container');
 const node =
 <Router history={browserHistory}>
   <Route path="/" component={App}>
     <IndexRoute component={Home}/>
-    <Route path="stuff" component={Stuff}/>
+    <Route path="notes">
+      <Route path="new" component={NoteForm}/>
+    </Route>
   </Route>
 </Router>;
 
